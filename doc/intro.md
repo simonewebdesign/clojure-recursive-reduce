ed4839e0f7b7ecb34f1cbf8d8cf4468e9ed77616
simonewebdesign <hello@simonewebdesign.it>
2016-07-11 19:49:12 +0100

    lein new app :rocket:

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-recursive-reduce
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_recursive_reduce/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-recursive-reduce.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-recursive-reduce.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
